10751b81ac6e7e78eda2831aaddfe4a5204830e0
Tino Ehrich <tino@bigpun.me>
2017-07-04 10:26:37 +0200

    initial

---
+++ example.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Facebook MoLo</title>
+</head>
+<body>
+
+<div>
+    <a href="#" id="fblogin" style="display:block;width:200px;font-size:20px;background:#4F5E97;color:#FFF;padding:10px;text-align: center;border-radius: 4px">FB LOGIN</a>
+    <div id="fbconnected" style="display:none;width:200px;font-size:20px;background:#45AF66;color:#FFF;padding:10px;text-align: center;border-radius: 4px">FB CONNECTED</div>
+    <p>Find the auth response in the console</p>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="facebook-molo.min.js?v=<?= time() ?>"></script>
+<script src="facebook-friends-graph.js?v=<?= time() ?>"></script>
+
+<script>
+    window.fbAsyncInit = function () {
+        var appId = '946621778709815';
+        var scope = 'public_profile,user_posts,user_photos';
+
+        FB.init({
+            appId: appId,
+            xfbml: true,
+            version: 'v2.9'
+        });
+
+        var fbMolo = new FacebookMoLo(FB, appId, scope, true);
+
+        var authCallback = function (response) {
+            console.log('AUTH CALLBACK', response);
+
+            if (response.status === 'connected') {
+                $('#fblogin').hide();
+                $('#fbconnected').css('display', 'block');
+
+                new FacebookFriends(FB, response.authResponse.userID).build(function (graph) {
+                    var total = graph.length;
+                    console.log('total graph items:', total);
+                    console.log('top 50 items:', graph.slice(0, 50));
+                });
+            }
+        };
+
+        if (fbMolo.isSuccessfulCallback()) {
+            fbMolo.authenticate(authCallback);
+        }
+
+        $('#fblogin').click(function (e) {
+            e.preventDefault();
+            fbMolo.authenticate(authCallback);
+        });
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+</body>
+</html>
---
+++ facebook-friends-graph.js
@@ -0,0 +1,99 @@
+var FacebookFriends = (function () {
+    function FriendsGraph(FB, userId) {
+        var app = {
+            config: {
+                edgeLimit: 100,
+                edgeFields: 'likes,comments',
+                points: {
+                    comment: 10,
+                    like: 5
+                }
+            },
+
+            graph: {},
+
+            buildGraph: function (callback) {
+                var that = this;
+
+                this.fetchEdges('/me/posts?fields=' + this.config.edgeFields + '&limit=' + this.config.edgeLimit, function (sortedGraph) {
+                    that.fetchEdges('/me/photos?type=uploaded&fields=' + that.config.edgeFields + '&limit=' + that.config.edgeLimit, callback);
+                });
+            },
+
+            fetchEdges: function (edge, callback) {
+                var that = this;
+
+                FB.api(edge, function (response) {
+                    that.handleEdgeResponse(response);
+                    callback(that.getSortedGraph());
+                });
+            },
+
+            handleEdgeResponse: function (response) {
+                if (typeof response.data !== 'undefined') {
+                    var that = this;
+
+                    $.each(response.data, function () {
+                        that.filterEdgeItemUserInteractions(this, 'likes');
+                        that.filterEdgeItemUserInteractions(this, 'comments');
+                    });
+                }
+            },
+
+            filterEdgeItemUserInteractions: function (item, interaction) {
+                if (typeof item[interaction] !== 'undefined') {
+                    var that = this;
+
+                    $.each(item[interaction].data, function () {
+                        var user = this;
+                        var points = that.config.points.like;
+
+                        if (interaction === 'comments') {
+                            user = this.from;
+                            points = that.config.points.comment;
+                        }
+
+                        if (user.id !== userId) {
+                            if (typeof that.graph[user.id] === 'undefined') {
+                                that.graph[user.id] = {
+                                    id: user.id,
+                                    points: 0,
+                                    name: user.name,
+                                    interactions: {
+                                        likes: [],
+                                        comments: []
+                                    }
+                                };
+                            }
+
+                            that.graph[user.id].points += points;
+                            that.graph[user.id].interactions[interaction].push(this);
+                        }
+                    });
+                }
+            },
+
+            getSortedGraph: function () {
+                var sortable = [];
+
+                $.each(this.graph, function () {
+                    sortable.push(this);
+                });
+
+                sortable.sort(function (a, b) {
+                    return a.points - b.points;
+                });
+
+                return sortable.reverse();
+            }
+        };
+
+        return {
+            build: function (callback) {
+                app.buildGraph(callback);
+            }
+        }
+    }
+
+    return FriendsGraph;
+})();
